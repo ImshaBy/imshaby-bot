fc34623a5e9a5923ada0b0e31f5780ad2a1c3f62
Andrei Misan <andrei.misan@gmail.com>
2021-05-03 17:16:37 +0300

    fix lang npe

--- src/bot.ts
+++ src/bot.ts
@@ -84,7 +84,9 @@ bot.hears(
 bot.hears(/(.*?)/, async (ctx: Context) => {
   logger.debug(ctx, 'Default handler has fired');
   const user = await User.findById(ctx.from.id);
-  await updateLanguage(ctx, user.language);
+  if (user){
+    await updateLanguage(ctx, user.language);
+  }
 
   const { mainKeyboard } = getMainKeyboard(ctx);
   return await ctx.reply(ctx.i18n.t('other.default_handler'), mainKeyboard);
