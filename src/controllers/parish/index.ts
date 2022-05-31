ed206a95b1aa429359ca2cf1da3cbd3526e8e2a8
Andrei Misan <andrei.misan@gmail.com>
2022-05-31 13:27:19 +0300

    remove avait

--- src/controllers/parish/index.ts
+++ src/controllers/parish/index.ts
@@ -23,7 +23,7 @@ parish.enter(async (ctx: SessionContext) => {
 
   if (userParishes && userParishes.length) {
     const {message_id} = await ctx.reply(ctx.i18n.t('scenes.parishes.list_of_parishes'), getParishesMenu(userParishes));
-    await ctx.session.cleanUpMessages.push(message_id);
+    ctx.session.cleanUpMessages.push(message_id);
     await ctx.reply(ctx.i18n.t('scenes.parishes.ask_for_details'), backKeyboard);
   } else {
     await ctx.reply('Error', backKeyboard);
