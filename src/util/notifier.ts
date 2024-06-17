e3d7a05a5471f06e15888fe08eb6fb3724599f91
Aliaksandra Kuzhelnaya <aliaksandra.kuzhelnaya@leverx.com>
2024-06-18 01:34:12 +0300

    adjust messages for expired parishes

--- src/util/notifier.ts
+++ src/util/notifier.ts
@@ -1,25 +1,31 @@
+import moment from 'moment';
 import logger from './logger';
 import { sleep } from './common';
 import User from '../models/User';
 import {telegram} from '../bot';
 import { parishesLookupByKey } from '../providers/search-providers';
 import { getAllNeedToUpdateParishKeys } from '../providers/search-providers/api';
-import { IParishResult } from '../providers/search-providers/parish-lookup';
+import { IParishResult, EXPIRED_PARISHES, IExpiredParish } from '../providers/search-providers/parish-lookup';
 import { CONFIG } from '../config';
 import { FmtString } from 'telegraf/typings/format';
 import { ExtraReplyMessage } from 'telegraf/typings/telegram-types';
 
 
 export async function checkNeeedToUpdateParishes() {
-    logger.debug(undefined, 'Starting to check need to updated parishes');
-    // run every friday, get masses for week & check needToUpdate (all not-..). If there is any unupdated mass ->
-    // fetch parish IDs -> get parish details & collect parish keys -> get users by parish keys
-    const neededParishKeys = await getAllNeedToUpdateParishKeys();
-    for (const parishKeyInfo of neededParishKeys) {
-        await sleep(0.5);
-        logger.debug(undefined, 'User is gonna to be notified for  , %O', parishKeyInfo);
-        await notifyAndUpdateUsersByParishKey(parishKeyInfo);
-    }
+  logger.debug(undefined, 'Starting to check need to updated parishes');
+  // run every friday, get masses for week & check needToUpdate (all not-..). If there is any unupdated mass ->
+  // fetch parish IDs -> get parish details & collect parish keys -> get users by parish keys
+  const neededParishes = await getAllNeedToUpdateParishKeys();
+  neededParishes.expiredParishes.forEach(async (parish) => {
+    await sleep(0.5);
+    logger.debug(undefined, 'User is gonna to be notified for  , %O', parish.key);
+    await notifyAndUpdateUsersByParishKey(parish, EXPIRED_PARISHES.EXPIRED);
+  });
+  neededParishes.almostExpiredParishes.forEach(async (parish) => {
+    await sleep(0.5);
+    logger.debug(undefined, 'User is gonna to be notified for  , %O', parish.key);
+    await notifyAndUpdateUsersByParishKey(parish, EXPIRED_PARISHES.ALMOST_EXPIRED);
+  });
 }
 
 export async function notifyGroupChatAboutParishChange(chatId: string, msg: string, msgThreadId: number) {
@@ -40,36 +46,45 @@ export async function notifyGroupChatAboutParishChange(chatId: string, msg: stri
     }
 }
 
-async function notifyAndUpdateUsersByParishKey(parishKey: string) {
-    const usersToNotify = await User.find({
-        observableParishKeys : parishKey
-    });
+async function notifyAndUpdateUsersByParishKey(parishData: IExpiredParish, type: EXPIRED_PARISHES) {
+  const usersToNotify = await User.find({
+    observableParishKeys: parishData.key,
+  });
 
-    const parish: IParishResult[] = await parishesLookupByKey(parishKey);
-    let parishName = parishKey;
-    if (parish && parish.length > 0) {
-        parishName = parish[0].title;
-    }
-    for (const user of usersToNotify) {
-        logger.debug(undefined, 'Notifying user %s about mass update for parish key %s', user.username, parishKey);
-        // TODO: move text to translations
+  for (const user of usersToNotify) {
+    logger.debug(
+      undefined,
+      'Notifying user %s about mass update for parish key %s',
+      user.username,
+      parishData.key
+    );
+    // TODO: move text to translations
 
-
+    const expirationTime = moment(parishData.lastConfirmRelevance).add(
+      parishData.updatePeriodInDays,
+      'days'
+    );
     const message =
       user.language === 'en'
-          ? `🎉 ${parishName} has to be updated!`
-          : `🎉 ${parishName} мае патрэбу ў аднаўленні раскладу!`;
+        ? `🎉 ${parishData.name} has to be updated!`
+        : type === EXPIRED_PARISHES.EXPIRED
+        ? `🎉 ${parishData.name} мае патрэбу ў аднаўленні раскладу!`
+        : `⌛️ Расклад Iмшаў парафii ${parishData.shortName}  стане неактуальным ${moment(
+            expirationTime
+          ).format('DD.MM.YYYY')} ў ${moment(expirationTime).format(
+            'HH:mm'
+          )}. Абнавi яго, калi ласка!`;
 
-        await sleep(0.5);
-        try {
-            async (ctx: any) => await ctx.scene.enter('parish')
-            await sendMessageWithErrorHandling( user._id, message);
-        } catch (e) {
-            logger.error(undefined, "Can't notify user about  reason: %O", e);
-        } finally {
-            // TODO: check if user blocked the bot and delete him from the DB
-        }
+    await sleep(0.5);
+    try {
+      async (ctx: any) => await ctx.scene.enter('parish');
+      await sendMessageWithErrorHandling(user._id, message);
+    } catch (e) {
+      logger.error(undefined, "Can't notify user about  reason: %O", e);
+    } finally {
+      // TODO: check if user blocked the bot and delete him from the DB
     }
+  }
 }
 
  export async function sendMessageWithErrorHandling (chatId: number | string,
