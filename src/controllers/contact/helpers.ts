4bd798d5a3f6d7210584385b17eb93c7bf8e20a8
Iryna Novysh <iryna.novysh@leverx.com>
2024-11-20 10:11:24 +0300

    add userId for notification

--- src/controllers/contact/helpers.ts
+++ src/controllers/contact/helpers.ts
@@ -9,7 +9,9 @@ import { telegram } from '../../bot';
 export async function sendMessage(ctx: any) {
   const msg = `Валанцёр: ${ctx.from.first_name} ${
     !!ctx.from.last_name ? ctx.from.last_name : ''
-  }\nUsername 🔎: @${ctx.from.username}\n\nПаведамленне 📨: ${ctx.message.text}`;
+  }\nUsername 🔎: @${!!ctx.from.username ? ctx.from.username : ''}\nUserID 🔑: ${
+    ctx.from.id
+  }\n\nПаведамленне 📨: ${ctx.message.text}`;
   const adminIds = process.env.ADMIN_IDS;
   const adminIdsArr = adminIds.split(',');
   for await (const adminId of adminIdsArr) {
