1173de408d20fed22c7ac261a767d27cf3189150
Juljan <y.misiukevich@gmail.com>
2025-10-13 12:07:22 +0300

    Add LocalDateMapper.ts to encapsulate local date names and formatting logic

--- src/controllers/schedule/actions.ts
+++ src/controllers/schedule/actions.ts
@@ -1,10 +1,6 @@
-import { sheduleByParishId, makeMassesActual} from '../../providers/search-providers';
-
-import { getParishScheduleControlMenu} from './helpers';
-
-const arrayOfWeekdays = ['Нд', 'Пн', 'Аў', 'Ср', 'Чц', 'Пт', 'Сб'];
-const arrayOfMonths = ['Студзеня', 'Лютага', 'Сакавіка', 'Красавіка', 'Мая', 'Чэрвеня', 'Ліпеня', 'Жніўня', 'Верасня', 'Кастрычніка', 'Лістапада', 'Снежня'];
-
+import { sheduleByParishId, makeMassesActual } from '../../providers/search-providers';
+import { getParishScheduleControlMenu } from './helpers';
+import { LocalDateMapper } from '../../util/LocalDateMapper';
 
 export const refreshScheduleAction = async (ctx: any) => {
 
@@ -21,43 +17,33 @@ export const refreshScheduleAction = async (ctx: any) => {
   }
 
   await ctx.answerCbQuery();
-
-    // await ctx.reply(`${successMsg}`);
-
 };
 
 
 export const parishSelectAction = async (ctx: any) => {
-    const parishName = ctx.i18n.t('scenes.parishes.chosen_parish', {
-        title: ctx.session.parish.title
+  const parishName = ctx.i18n.t('scenes.parishes.chosen_parish', {
+    title: ctx.session.parish.title
+  });
+
+  let replyMsg = `\n ${parishName}`;
+
+  const massDays = await sheduleByParishId(ctx.session.parish.id);
+
+  for (const massDay of massDays) {
+    const parts = massDay.date.split('/');
+    const massDate: Date = new Date(Number.parseInt(parts[2]), Number.parseInt(parts[0]) - 1, Number.parseInt(parts[1]));
+
+    const mapper = new LocalDateMapper();
+    let scheduleForDay = mapper.format(massDate) + ': ';
+
+    massDay.massHours.forEach(function(hour) {
+      scheduleForDay += ` ${hour}`;
     });
+    replyMsg += `\n ${scheduleForDay}`;
+  }
+  await ctx.reply(replyMsg);
 
-    let replyMsg = `\n ${parishName}`;
-    // await ctx.reply(`\n ${parishName}`);
-
-    const massDays = await sheduleByParishId(ctx.session.parish.id);
-
-    for ( const massDay of massDays) {
-        const parts = massDay.date.split('/');
-        const massDate: Date = new Date(Number.parseInt(parts[2]), Number.parseInt (parts[0]) - 1, Number.parseInt(parts[1]));
-
-        let sheduleForDay = `${arrayOfWeekdays[massDate.getDay()]}, ${massDate.getDate()} ${arrayOfMonths[massDate.getMonth()]}` + ': ';
-        massDay.massHours.forEach(function(hour) {
-            sheduleForDay += ` ${hour}`;
-        });
-        replyMsg  += `\n ${sheduleForDay}`;
-
-    // await ctx.reply(sheduleForDay);
-    }
-    await ctx.reply(replyMsg);
-
-    // await ctx.editMessageText(replyMsg);
-
-    // const authCode = await await getPasswordlessCode(ctx.session.user.name);
-
-    const authCode = 'codeMock';
-    const {message_id} = await ctx.reply(ctx.i18n.t('scenes.parishes.cta_update'), getParishScheduleControlMenu(ctx, authCode));
-    await ctx.session.cleanUpMessages.push(message_id);
-
-    // await ctx.answerCbQuery();
+  const authCode = 'codeMock';
+  const { message_id } = await ctx.reply(ctx.i18n.t('scenes.parishes.cta_update'), getParishScheduleControlMenu(ctx, authCode));
+  await ctx.session.cleanUpMessages.push(message_id);
 };
